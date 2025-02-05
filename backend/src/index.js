faa115d60f86b95db67088c2d13e4b1b031d315a
tanukanwar14 <tanu.20225096@mnnit.ac.in>
2025-02-05 15:33:07 +0530

    Initial commit

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,17 +1,31 @@
 
 import express from "express";
 import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
 import authRoutes from "./routes/auth.route.js";
+import messageRoutes from "./routes/message.route.js";
 import { connectDB } from "./lib/db.js";
+import cors from "cors";
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5001;
 
-app.use(express.json());
+app.use(express.json({ limit: '50mb' }));        // here we increase the limit
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
+
+app.use(cookieParser());
+
+app.use(
+    cors({
+      origin: "http://localhost:5173",
+      credentials: true,
+    })
+  );
 
 
 app.use("/api/auth", authRoutes);
+app.use("/api/messages", messageRoutes);
 app.listen(PORT, ()=>{
     console.log("server is running on port:" + PORT);
     connectDB();
